8d5ee8cf93c6220d323c76ed0f4c44af8d146891
Cory Watson <cryo.warden@gmail.com>
2023-12-29 16:27:25 -0800

    Remove custom ECS crates.

--- ecs/src/lib.rs
+++
@@ -1,6 +0,0 @@
-extern crate ecs_macros;
-
-pub mod components;
-pub mod systems;
-#[macro_use]
-pub mod world;
--- ecs/src/systems/mod.rs
+++
@@ -1,5 +0,0 @@
-mod gravity_system;
-mod movement_system;
-
-pub use gravity_system::GravitySystem;
-pub use movement_system::MovementSystem;
